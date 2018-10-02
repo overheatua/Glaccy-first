a9801f54442652421e9107e1e786a7cd347c74f3
Roman Bychkov <overheatua@gmail.com>
2018-10-03 02:13:14 +0300

    bug-fix index.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 50.116477, lng: 30.636405},
+    zoom: 17,
+    disableDefaultUI: true
+  });
+
+  var image = 'img/map-pin.png';
+  var beachMarker = new google.maps.Marker({
+    position: {lat: 50.116477, lng: 30.636405},
+    map: map,
+    icon: image
+  });
+}
+
 var link = document.querySelector(".contact-link");
 var modal = document.querySelector(".modal-contact-form");
 var close = document.querySelector(".modal-contact-form .close-btn");
